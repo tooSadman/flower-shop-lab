7cbbdfa2e486034fa22de7891757b5ae55351b81
tooSadman <mykola.kb@gmail.com>
2020-06-08 01:33:33 +0300

    Added func to get flower by flower_name

--- internal/flower/flower.go
+++ internal/flower/flower.go
@@ -69,3 +69,26 @@ func GetFlower(id int, db *sql.DB) Flower {
 
 	return flower
 }
+
+func GetFlowerByName(name string, db *sql.DB) Flower {
+	var id int
+	var picture string
+	var price int
+
+	err := db.QueryRow(`
+	SELECT id, flower_name, picture, price 
+	FROM flowers where flower_name = $1
+	`, name,
+	).Scan(&id, &name, &picture, &price)
+	if err != nil {
+		log.Fatal(err)
+	}
+	flower := Flower{
+		ID:      id,
+		Name:    name,
+		Picture: picture,
+		Price:   price,
+	}
+
+	return flower
+}
