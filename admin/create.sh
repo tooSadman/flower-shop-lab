664befda6eeb0b1c74ba75f866f180290290bc86
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2020-06-14 17:39:01 +0300

    linux fixes

--- admin/create.sh
+++ admin/create.sh
@@ -1,8 +1,8 @@
 docker network create flowershop
 docker run --name flowershop_db -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 docker exec -ti flowershop_db hostname -i
-pause
-docker cp pg.sql flowershop_db:/ 
+echo "Press Enter to continue"; read
+docker cp ../migrations/20200529014155_create_db.up.sql flowershop_db:/ 
 docker cp pgsql.sql flowershop_db:/
 docker exec -ti flowershop_db bash -c "PGPASSWORD=mysecretpassword psql -U postgres -f pgsql.sql && PGPASSWORD=mysecretpassword psql -d flowershop -U postgres -f pg.sql"
 docker build -t flowershop_admin -f Dockerfile_admin .
