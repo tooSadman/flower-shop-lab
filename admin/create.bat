f7c5b6386f6575c40fd4d5ab0fc55eb461e03fc6
Fufe1ok <32967290+Fufe1ok@users.noreply.github.com>
2020-06-14 17:30:17 +0300

    added files for creating and deploying admin pages

---
+++ admin/create.bat
@@ -0,0 +1,9 @@
+docker network create flowershop
+docker run --name flowershop_db -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker exec -ti flowershop_db hostname -i
+pause
+docker cp pg.sql flowershop_db:/ 
+docker cp pgsql.sql flowershop_db:/
+docker exec -ti flowershop_db bash -c "PGPASSWORD=mysecretpassword psql -U postgres -f pgsql.sql && PGPASSWORD=mysecretpassword psql -d flowershop -U postgres -f pg.sql"
+docker build -t flowershop_admin -f Dockerfile_admin .
+docker run --net flowershop --name flowershop_admin -dti -p 3000:3000 flowershop_admin
---
+++ admin/pg.sql
@@ -0,0 +1,46 @@
+--Creating users tables
+create table users (
+  id serial not null unique,
+  email varchar not null primary key,
+  encrypted_password varchar not null,
+  username varchar not null unique
+);
+
+--Creating flowers table
+create table flowers (
+  id serial not null unique,
+  flower_name varchar not null primary key,
+  picture varchar not null,
+  price int not null
+);
+
+--Creating orders table
+create table orders (
+  id serial not null unique,
+  flower_name varchar references flowers(flower_name),
+  customer varchar references users(email),
+  price int not null
+);
+
+--Inserting values into table users
+insert into users 
+  (id, email, encrypted_password, username)
+values
+  (1, 'john@gmail.com', 'john', 'john'),
+  (2, 'stashkiv@gmail.com', 'stashkiv', 'stashkiv'),
+  (3, 'fufelok@dasdasd', 'yurii', 'yurii'),
+  (4, 'mykola@gmail.com', 'mykola', 'mykola');
+ 
+
+--Inserting values into table flowers
+insert into flowers 
+  (id, flower_name, picture, price)
+values 
+  (1, 'Rose', 'https://www.springintheair.com/wp-content/uploads/bfi_thumb/red-roses-50-set-compressor-n5oucgj8yzgm0fcs1j2el4wxysgxatrjyhwsk99vtk.jpg', 20),
+  (2, 'Tulip', 'https://www.whiteflowerfarm.com/mas_assets/cache/image/6/3/6/0/25440.Jpg', 15),
+  (3, 'Iris', 'https://www.gardenia.net/storage/app/public/guides/detail/83847060_mOptimized.jpg', 18),
+  (4, 'Peony', 'https://florium.ua/media/catalog/product/cache/2/file/9df78eab33525d08d6e5fb8d27136e95/p/a/paeonia_patio_peony_madrid_03510_1_1.jpg', 17),
+  (5, 'Crocus', 'https://s3.amazonaws.com/cdn0.brecks.com/images/500/63942.jpg', 25),
+  (6, 'Lily', 'https://s3.amazonaws.com/cdn0.brecks.com/images/500/73689A.jpg', 30),
+  (7, 'Daisy', 'https://images-na.ssl-images-amazon.com/images/I/61jtIbIPbgL._AC_SL1005_.jpg', 16),
+  (8, 'Poppy', 'https://cdn.shopify.com/s/files/1/1186/5156/products/Red-Poppy_1024x1024.jpeg?v=1456914865', 19);
